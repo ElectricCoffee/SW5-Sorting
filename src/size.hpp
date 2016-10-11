30b1d8050cb71b4c19eaf9ebec424f10b2d7b6da
Niko Lepka <slench102+git@gmail.com>
2016-10-11 16:06:53 +0200

    move code to the correct place in the guard
    Include guards are important, place your code **between** #define and #endif

--- src/size.hpp
+++ src/size.hpp
@@ -1,8 +1,6 @@
 // The header file of size
 #ifndef size_hpp
 #define size_hpp
-// code goes here
-#endif
 
 class size : private sensor {
 private:
@@ -14,3 +12,5 @@ public:
   unsigned int get_size();
   void check_measuring;
 }
+
+#endif
