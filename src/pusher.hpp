b67479f7eda30f78b2eef907d89032b65e5409f8
somethingstuff <anders2213@hotmail.com>
2016-10-14 09:01:41 +0200

    Added pusher functionality

--- src/pusher.hpp
+++ src/pusher.hpp
@@ -1,5 +1,17 @@
 // The header file of pusher
 #ifndef pusher_hpp
 #define pusher_hpp
-// code goes here
+class pusher {
+  public:
+    sensor photo_sensor; //it needs one, but maybe could be an input var in detect brick instead? just need pin number
+    sensor servo_sensor; //or maybe just be some ints instead? might just be objects for objects sake. or maybe inherit
+    void open();
+    void close();
+    bool detect_brick(); //returns to ensure the controllers knows a new brick appeared
+    pusher(int, int); //constructor which takes the pins as input, first one photo
+
+  private:
+    Servo the_servo; //servo object from arduino servo library. creates its own in construction
+
+}
 #endif
