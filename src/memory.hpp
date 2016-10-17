5eb2279266bb235e579277b3224813344a409788
Niko Lepka <slench102+git@gmail.com>
2016-10-17 09:52:39 +0200

    Update memory.hpp

--- src/memory.hpp
+++ src/memory.hpp
@@ -8,11 +8,11 @@
 
 class memory {
   public:
-    void enque(brick, short); //short is for which conveyor it is
-    brick deque(short);
+    void enqueue(brick, short); //short is for which conveyor it is
+    brick dequeue(short);
     void add_conveyor();
     void remove_conveyor(short); //short is which conveyor to remove
     memory(short); //constructor takes as input the amount of conveyors to start with
   private:
-    std::vector<std::deque<brick> > _queue;
+    std::vector<std::deque<brick>* > _queue;
 #endif
