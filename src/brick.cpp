302e5151ef1404e0cb9d656436ed0f4320afc8c2
electric coffee <slench102@gmail.com>
2016-10-18 11:58:59 +0200

    add == to brick
    also changes formatting

--- src/brick.cpp
+++ src/brick.cpp
@@ -1,2 +1,16 @@
 // The implementation file of brick
 #include "brick.hpp"
+
+brick::brick(int col, int x, int y, int z) {
+  color  = col;
+  size_x = x;
+  size_y = y;
+  size_z = z;
+}
+
+bool brick::operator ==(const brick &that) const {
+  return color  == that.color
+    &&   size_x == that.size_x
+    &&   size_y == that.size_y
+    &&   size_z == that.size_z;
+}
