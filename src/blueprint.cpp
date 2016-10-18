da4142f71daa2bf27ee33adfb63a1f934a72d087
electric coffee <slench102@gmail.com>
2016-10-18 12:13:16 +0200

    implement is_brick_useful

--- src/blueprint.cpp
+++ src/blueprint.cpp
@@ -5,8 +5,6 @@ blueprint::blueprint() {
   // add initialiser code here
 }
 
-bool blueprint::is_brick_useful(brick b) {
-  // insert relevant checks here
 blueprint::~blueprint() {
   // add destruction code here
 }
@@ -25,3 +23,20 @@ void blueprint::pop_front() {
   _registered_bricks.pop_front();
 }
 
+/**
+ * Checks if a brick is useful
+ * @param brick the brick to be checked
+ */
+bool blueprint::is_brick_useful(brick a_brick) {
+  std::deque<brick>::iterator deque_iterator;
+  
+  for (deque_iterator  = _registered_bricks.begin();
+       deque_iterator != _registered_bricks.end();
+       deque_iterator++) {
+    if (*deque_iterator == a_brick) {
+      return true;
+    } // else continue looking
+  }
+
+  return false;
+}
