ae00a59e1d8069b8a4d9b5d3f47f65b981d125ed
Niko Lepka <slench102+git@gmail.com>
2016-10-19 13:08:10 +0200

    Update sensor.hpp

--- src/sensor.hpp
+++ src/sensor.hpp
@@ -1,9 +1,8 @@
 // The header file of sensor
 #ifndef sensor_hpp
 #define sensor_hpp
-class sensor {
-  public:
-    short pin;
-    sensor(char); //for initialising it
+struct sensor {
+  char pin;
+  sensor(char); //for initialising it
 }
 #endif
