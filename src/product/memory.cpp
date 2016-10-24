beddfbd126484d855f2bd3ddff56aa1154150f38
electric coffee <slench102@gmail.com>
2016-10-24 13:05:51 +0200

    fix type warnings

--- src/product/memory.cpp
+++ src/product/memory.cpp
@@ -19,7 +19,7 @@ memory::memory(short amount_conveyors) {
  * @param a_conveyor_number which conveyor it gets put on
  */
 void memory::enqueue(brick a_brick, short a_conveyor_number) {
-  if(_queue.size() >= a_conveyor_number) { //to ensure proper access
+  if(_queue.size() >= (size_t) a_conveyor_number) { //to ensure proper access
     _queue[a_conveyor_number]->push_back(a_brick);
   }
 }
@@ -30,13 +30,12 @@ void memory::enqueue(brick a_brick, short a_conveyor_number) {
  * @param a_conveyor_number which conveyor to take on from
  */
 brick memory::dequeue(short a_conveyor_number) {
-  if(_queue.size() >= amount_conveyors) {
-    brick temp_brick = (*_queue[a_conveyor_number])[0]; 
+  if (_queue.size() >= (size_t) a_conveyor_number) {
+    brick temp_brick = (*_queue[a_conveyor_number])[0];
     _queue[a_conveyor_number]->erase( _queue[a_conveyor_number]->begin() ); //deletes the first pos and should resize it
     return temp_brick;
-  } 
-  else {
-    return null;
+  } else {
+    return brick::empty_brick();
   }
 }
 
@@ -53,7 +52,7 @@ void memory::add_conveyor() {
  * \todo this probably needs testing to see if both delete and erase works this way
  */
 void memory::remove_conveyor(short a_conveyor_number) { //not saving any of the deleted data, dont run on existing conveyors
-  if(_queue.size() >= a_conveyor_number) {
+  if(_queue.size() >= (size_t) a_conveyor_number) {
     delete( _queue[a_conveyor_number] ); //this should delete what the pointers point at
     _queue.erase( _queue.begin() + a_conveyor_number ); //this deletes the pointer and resizes the vector
   }
