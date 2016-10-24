fa6ad7a7f9b8adde9fc0dc06de60aa85c8168746
electric coffee <slench102@gmail.com>
2016-10-24 14:44:36 +0200

    hey, what's that over there?!

--- src/product/color.hpp
+++ src/product/color.hpp
@@ -1,5 +1,11 @@
 // The header file of color
 #ifndef color_hpp
 #define color_hpp
-// code goes here
+#include <Arduino.h>
+#include "sensor.hpp"
+
+struct color : public sensor {
+  int get_color();
+};
+
 #endif
