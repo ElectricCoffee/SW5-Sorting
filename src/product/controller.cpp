a38cf8fd5fc97652c2ce6b83cfd8abd17dc1cd73
electric coffee <slench102@gmail.com>
2016-10-25 10:33:47 +0200

    add initial controller code

--- src/product/controller.cpp
+++ src/product/controller.cpp
@@ -1,2 +1,32 @@
 // The implementation file of controller
 #include "controller.hpp"
+
+/**
+ * Registers bricks that have been scanned to keep track of the bricks in the system
+ * @param brk the brick to be added
+ */
+void controller::register_brick(brick brk) {
+  _registered_bricks.push_back(brk);
+}
+
+/**
+ * Registers a component in the set of available components
+ * If a component already exists, it won't be added to the set.
+ * @param comp the component to be added to the set.
+ */
+void controller::register_component(component comp) {
+  std::vector<component>::iterator it;
+  bool component_exists = false;
+
+  for (it  = _registered_components.begin(); it != _registered_components.end(); it++) {
+    if (typeid(*it).hash_code() != typeid(comp).hash_code()) {
+      continue; // if the types don't match, check the next component
+    } else if (*it == comp) {
+      coponent_exists = true;
+    }
+  }
+
+  if (!component_exists) {
+    _registered_components.push_back(comp);
+  }
+}
--- src/product/controller.hpp
+++ src/product/controller.hpp
@@ -1,5 +1,22 @@
 // The header file of controller
 #ifndef controller_hpp
 #define controller_hpp
-// code goes here
+#include <Arduino.h>
+#include <StandardCplusplus.h>
+#include <vector>
+#include <list>
+#include "brick.hpp"
+#include "component.hpp"
+
+class controller {
+private:
+  std::list<brick> _registered_bricks;
+  std::vector<component> _registered_components;
+public:
+  controller();
+  ~controller();
+  void register_brick(brick);
+  void register_component(component);
+};
+
 #endif
