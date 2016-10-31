bdafd13e8192cf934990eba56208d2d296dcbb2e
Niko Lepka <slench102+git@gmail.com>
2016-10-31 19:31:37 +0100

    implement controller's constructor and destructor
    got a compiler error because I forgot earlier, oops

--- src/product/controller.cpp
+++ src/product/controller.cpp
@@ -1,6 +1,9 @@
 // The implementation file of controller
 #include "controller.hpp"
 
+controller::controller() {}
+controller::~controller() {}
+
 /**
  * Registers bricks that have been scanned to keep track of the bricks in the system
  * @param brk the brick to be added
