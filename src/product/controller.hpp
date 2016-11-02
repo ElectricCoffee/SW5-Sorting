c2f54415bf9d1377748ee70bf7102e869a2e6438
Niko Lepka <slench102+git@gmail.com>
2016-11-02 15:52:03 +0100

    add more includes and private variables to controller
    Adds imports for deque, iterator, and sensor
    Adds _sensor_buffer, and _registered_sensors

--- src/product/controller.hpp
+++ src/product/controller.hpp
@@ -5,13 +5,18 @@
 #include <StandardCplusplus.h>
 #include <vector>
 #include <list>
+#include <deque>
+#include <iterator>
 #include "brick.hpp"
 #include "component.hpp"
+#include "sensor.hpp"
 
 class controller {
 private:
   std::list<brick> _registered_bricks;
+  std::vector<std::deque<brick> > _sensor_buffers;
   std::vector<component> _registered_components;
+  std::vector<sensor> _registered_sensors;
 public:
   controller();
   ~controller();
