2b9ee4125764e7939202614ca3b26a07196da97f
Niko Lepka <slench102+git@gmail.com>
2016-11-03 14:04:43 +0100

    change short to uint8_t

--- src/product/memory.cpp
+++ src/product/memory.cpp
@@ -5,9 +5,9 @@
  * sets the queue at the proper starting size and creates the correct amount of conveyors
  * @param amount_conveyors how many conveyors that needs to be added
  */
-memory::memory(short amount_conveyors) {
+memory::memory(uint8_t amount_conveyors) {
   _queue.resize(amount_conveyors);
-  for(int i = 0; i < amount_conveyors; i++) {
+  for (uint8_t i = 0; i < amount_conveyors; i++) {
     add_conveyor(); //initialization
   }
 }
@@ -18,8 +18,8 @@ memory::memory(short amount_conveyors) {
  * @param a_brick the brick to be added
  * @param a_conveyor_number which conveyor it gets put on
  */
-void memory::enqueue(brick a_brick, short a_conveyor_number) {
-  if(_queue.size() >= (size_t) a_conveyor_number) { //to ensure proper access
+void memory::enqueue(brick a_brick, uint8_t a_conveyor_number) {
+  if (_queue.size() >= (size_t) a_conveyor_number) { //to ensure proper access
     _queue[a_conveyor_number]->push_back(a_brick);
   }
 }
@@ -29,10 +29,10 @@ void memory::enqueue(brick a_brick, short a_conveyor_number) {
  * shrinks the queue to use less memory
  * @param a_conveyor_number which conveyor to take on from
  */
-brick memory::dequeue(short a_conveyor_number) {
+brick memory::dequeue(uint8_t a_conveyor_number) {
   if (_queue.size() >= (size_t) a_conveyor_number) {
     brick temp_brick = (*_queue[a_conveyor_number])[0];
-    _queue[a_conveyor_number]->erase( _queue[a_conveyor_number]->begin() ); //deletes the first pos and should resize it
+    _queue[a_conveyor_number]->erase(_queue[a_conveyor_number]->begin()); //deletes the first pos and should resize it
     return temp_brick;
   } else {
     return brick::empty_brick();
@@ -51,7 +51,7 @@ void memory::add_conveyor() {
  * @param a_conveyor_number which conveyor to remove
  * \todo this probably needs testing to see if both delete and erase works this way
  */
-void memory::remove_conveyor(short a_conveyor_number) { //not saving any of the deleted data, dont run on existing conveyors
+void memory::remove_conveyor(uint8_t a_conveyor_number) { //not saving any of the deleted data, dont run on existing conveyors
   if(_queue.size() >= (size_t) a_conveyor_number) {
     delete( _queue[a_conveyor_number] ); //this should delete what the pointers point at
     _queue.erase( _queue.begin() + a_conveyor_number ); //this deletes the pointer and resizes the vector
--- src/product/memory.hpp
+++ src/product/memory.hpp
@@ -1,20 +1,21 @@
 // The header file of memory
 #ifndef memory_hpp
 #define memory_hpp
+#include <Arduino.h>
 #include <StandardCplusplus.h>
 #include <vector>
 #include <deque>
 #include "brick.hpp"
 
 class memory {
-  public:
-    void enqueue(brick, short); //short is for which conveyor it is
-    brick dequeue(short);
-    void add_conveyor();
-    void remove_conveyor(short); //short is which conveyor to remove
-    memory(short); //constructor takes as input the amount of conveyors to start with
-  private:
-    std::vector<std::deque<brick>* > _queue;
-    ~memory(); //deconstructor
+private:
+  std::vector<std::deque<brick>*> _queue;
+  ~memory();
+public:
+  void enqueue(brick, uint8_t);
+  brick dequeue(uint8_t);
+  void add_conveyor();
+  void remove_conveyor(uint8_t);
+  memory(uint8_t);
 };
 #endif
