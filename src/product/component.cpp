9cb2d046aab4357fa32b3ee6ed793fa6fe0ec8f1
Niko Lepka <slench102+git@gmail.com>
2016-11-08 14:36:39 +0100

    remove component::detect_brick
    forgot to earlier

--- src/product/component.cpp
+++ src/product/component.cpp
@@ -5,10 +5,6 @@ component::component(uint8_t a_pin) {
   pin = a_pin;
 }
 
-bool component::detect_brick() {
-  return digitalRead(pin) == HIGH;
-}
-
 bool component::operator ==(const component &comp) const {
   // if they share the same pin, they're probably the same component
   return pin == comp.pin;
