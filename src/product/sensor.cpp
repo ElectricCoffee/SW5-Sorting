61ef3775bec994539d40ae5d00eb1dc75f0dbb0f
Niko Lepka <slench102+git@gmail.com>
2016-11-14 09:52:15 +0100

    make sensor purely virtual

--- src/product/sensor.cpp
+++ src/product/sensor.cpp
@@ -3,12 +3,17 @@
 /**
  * initialises the pin value, just there to make it easier
  */
-sensor::sensor(uint8_t a_pin) : component(a_pin) { }
+//sensor::sensor(uint8_t a_pin) : component(a_pin) { }
 
-brick sensor::get_brick_data() {
-  // Please override this method.
-  return brick::empty_brick();
-}
+//brick sensor::get_brick_data() {
+//  // Please override this method.
+//  return brick::empty_brick();
+//}
+
+/*
+bool sensor::detect_brick() {
+  return false; // override this please
+}*/
 
 /*
 bool sensor::operator==(const sensor s) const {
--- src/product/sensor.hpp
+++ src/product/sensor.hpp
@@ -2,11 +2,12 @@
 #ifndef sensor_hpp
 #define sensor_hpp
 #include <Arduino.h>
+#include "brick_detector.hpp"
 #include "component.hpp"
 #include "brick.hpp"
 
-struct sensor : public component {
-  sensor(uint8_t); //for initialising it
-  virtual brick get_brick_data();
+struct sensor : public component, public brick_detector {
+  //sensor(uint8_t); //for initialising it
+  virtual brick get_brick_data() = 0;
 };
 #endif
