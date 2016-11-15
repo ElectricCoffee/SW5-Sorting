9a876dc86f542a46af870d5f22f0c400fbdf3fab
Niko Lepka <slench102+git@gmail.com>
2016-11-15 11:53:43 +0100

    add constructor to sensor

--- src/product/sensor.hpp
+++ src/product/sensor.hpp
@@ -9,6 +9,7 @@
 struct sensor : public component, public brick_detector {
   // setting a function = 0, makes it a "pure virtual function"
   // don't touch it.
+  sensor(uint8_t pin) : component(pin) {}
   virtual brick        get_brick_data()  = 0;
   virtual unsigned int check_measuring() = 0;
   virtual unsigned int start_measuring() = 0;
