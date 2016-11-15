72e1020ae7468a9024745584af25922cb33f2ebd
Niko Lepka <slench102+git@gmail.com>
2016-11-15 10:04:04 +0100

    remove useless comments from sensor

--- src/product/sensor.cpp
+++ src/product/sensor.cpp
@@ -1,22 +1,3 @@
 // The implementation file of sensor
 #include "sensor.hpp"
-/**
- * initialises the pin value, just there to make it easier
- */
-//sensor::sensor(uint8_t a_pin) : component(a_pin) { }
-
-//brick sensor::get_brick_data() {
-//  // Please override this method.
-//  return brick::empty_brick();
-//}
-
-/*
-bool sensor::detect_brick() {
-  return false; // override this please
-}*/
-
-/*
-bool sensor::operator==(const sensor s) const {
-  return pin == s.pin;
-}
-*/
+// Sensor is a pure virtual class, i.e. an interface, so nothing's here
