f7c1d6a0a8baed0c706a2a93663201ccc130561a
Niko Lepka <slench102+git@gmail.com>
2016-11-16 14:07:19 +0100

    add conveyor motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
 #include <Arduino.h>
 #include "product/controller.hpp"
+#include "product/motor.hpp"
 #include "product/motor_pusher.hpp"
 #include "product/color.hpp"
 #include "product/size.hpp"
 
 #ifndef UNIT_TEST // important line, do not remove
+#define NO_PIN 0
 
 controller contr;
-color color_sensor(0);
+color color_sensor(NO_PIN);
 //size  size_sensor;
-motor_pusher pusher1(0, M4, 22);
+motor_pusher pusher1(NO_PIN, M4, 22);
+motor motor1(128, M1, NO_PIN);
 
 void setup() {
+  motor1.run_forward();
   contr.register_sensor(color_sensor);
   contr.register_component(pusher1);
 }
