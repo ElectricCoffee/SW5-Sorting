e2ca87dd951c45384e49863f9f25d07ba9a426a5
Daniel-San <dond9220@gmail.com>
2016-11-22 15:52:55 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,26 @@
 #define NO_PIN 0
 
 controller contr;
-color color_sensor(NO_PIN);
-//size  size_sensor;
+color color_sensor(53);
+size  size_sensor(A8);
 motor_pusher pusher1(NO_PIN, M4, 22);
-motor motor1(128, M1, NO_PIN);
+motor motor1(128, M2, NO_PIN);
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("test");
   motor1.run_forward();
-  contr.register_sensor(color_sensor);
-  contr.register_component(pusher1);
+  contr.register_sensor(size_sensor);
+  //contr.register_sensor(color_sensor);
+  //contr.register_component(pusher1);
 }
 
 void loop() {
   contr.read_sensors();
+  pinMode(24, INPUT);
+  pinMode(A8, INPUT);
+  //Serial.println(analogRead(A8));
+//  Serial.println(analogRead(A8));
 }
 
 #endif // important line, do not remove
