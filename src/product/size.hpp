fdb8abfeb2d300305c2e598585ab8fa95cdf1102
Daniel-San <dond9220@gmail.com>
2016-11-22 15:52:08 +0100

    update size header

--- src/product/size.hpp
+++ src/product/size.hpp
@@ -11,13 +11,14 @@ private:
   void stop_measuring();
   unsigned long _current_time;
   unsigned long _break_time;
-  unsigned int _newest_size;
+  int _newest_size;
 public:
   size(uint8_t);
   unsigned int get_size();
-  void check_measuring();
+  brick check_measuring();
   void stop_measuring_temporarily();
   void continue_measuring();
+  virtual brick get_brick_data();
 };
 
 #endif
