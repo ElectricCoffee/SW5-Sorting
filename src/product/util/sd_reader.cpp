629121e02cf5441a78f5a3d62427d219290d6918
Niko Lepka <slench102+git@gmail.com>
2016-12-05 15:41:23 +0100

    remove useless comments

--- src/product/util/sd_reader.cpp
+++ src/product/util/sd_reader.cpp
@@ -15,38 +15,6 @@ sd_reader::~sd_reader() {
   }
 }
 
-// status sd_reader::read_file_to_string(char *output) {
-//   const char *method_name = "sd_reader::read_file_to_string";
-//   std::deque<char> de_buffer;
-//   std::deque<char>::iterator it;
-//   if (!_file) {
-//     return status::failure(method_name, "Could not open the file.");
-//   } else {
-//     while (_file.available()) {
-//       de_buffer.push_back((char)_file.read());
-//     }
-//
-//     int file_len = de_buffer.size();
-//
-//     void *success = realloc(output, file_len * sizeof(char*));
-//
-//     if (success != NULL) {
-//       int i;
-//       for (i = 0, it = de_buffer.begin(); it != de_buffer.end(); it++, i++) {
-//         output[i] = *it;
-//         Serial.println(i);
-//       }
-//     } else {
-//       Serial.println("fail realloc str");
-//       return status::failure(method_name, "Failed to reallocate string");
-//     }
-//     Serial.println("starting output writing");
-//     Serial.println(output);
-//     Serial.println("finished writing output");
-//     return status::success();
-//   }
-// }
-
 status sd_reader::read_file_to_string(char *output) {
   //COL:02430737 LEN:00005254
   const char *method_name = "sd_reader::read_file_to_string";
@@ -57,11 +25,6 @@ status sd_reader::read_file_to_string(char *output) {
   } else {
     String res = _file.readString();
     Serial.println(res);
-    // while (_file.available()) {
-    //   _file.read(buffer, LINE_LEN);
-    //   strcat(output, buffer);
-    //   _file.readS
-    // }
     return status::success();
   }
 }
