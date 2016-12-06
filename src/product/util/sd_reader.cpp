cfc5ba9f72dda9ae406445f48c2f107bf4e26760
Niko Lepka <slench102+git@gmail.com>
2016-12-06 12:10:21 +0100

    add Arduino String to char array conversion logic in read_file_to_string

--- src/product/util/sd_reader.cpp
+++ src/product/util/sd_reader.cpp
@@ -18,13 +18,27 @@ sd_reader::~sd_reader() {
 status sd_reader::read_file_to_string(char *output) {
   //COL:02430737 LEN:00005254
   const char *method_name = "sd_reader::read_file_to_string";
-  //char buffer[LINE_LEN + 1] = "";
 
   if (!_file) {
     return status::failure(method_name, "Failed to open the file.");
   } else {
-    String res = _file.readString();
-    Serial.println(res);
+    String str = _file.readString();
+    int strlen = str.length();
+
+    if (strlen == 0) {
+      return status::failure(method_name, "Read 0 bytes from file, is this right?");
+    }
+
+    void *result_ptr = realloc(output, (strlen + 1) * sizeof(char *));
+
+    if (result_ptr == NULL) {
+      return status::failure(method_name, "Failed to resize the output");
+    }
+
+    str.toCharArray(output, strlen);
+
+    Serial.println(output);
+
     return status::success();
   }
 }
