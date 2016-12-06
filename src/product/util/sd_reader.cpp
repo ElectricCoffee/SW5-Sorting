4b7ba9d5eb7214cfc6cd2e2486c52a4b11bff2a8
Niko Lepka <slench102+git@gmail.com>
2016-12-06 12:37:42 +0100

    add documentation to sd_reader

--- src/product/util/sd_reader.cpp
+++ src/product/util/sd_reader.cpp
@@ -1,6 +1,11 @@
 // The implementation of file sd_reader
 #include "sd_reader.hpp"
 
+/**
+ * Creates a file pointer to a file on the SD card
+ * @param file_name is the name of the input file.
+ * Note that the file name should be in all caps for it to be read.
+ */
 sd_reader::sd_reader(const char *file_name) {
   pinMode(SS_PIN, OUTPUT);
 
@@ -9,14 +14,22 @@ sd_reader::sd_reader(const char *file_name) {
  Serial.println("Success!");
 }
 
+/**
+ * Makes sure the file is closed again if it exists
+ */
 sd_reader::~sd_reader() {
   if (_file) {
     _file.close();
   }
 }
 
+/**
+ * Reads the contents of the SD Reader's file handle to a string.
+ * @param output is the output-string as an output-parameter.
+ * @returns a status::success if successful,
+ * or a status::failure if something went wrong
+ */
 status sd_reader::read_file_to_string(char *output) {
-  //COL:02430737 LEN:00005254
   const char *method_name = "sd_reader::read_file_to_string";
 
   if (!_file) {
@@ -31,6 +44,9 @@ status sd_reader::read_file_to_string(char *output) {
     return status::failure(method_name, "Read 0 bytes from file, is this right?");
   }
 
+  // resize and nullify the output buffer before copying data into it
+  // this is done to ensure there's enough room for the read string,
+  // and that there's a null-terminator at the end of the string.
   void *result_ptr = realloc(output, bytes);
   memset(output, '\0', bytes);
 
