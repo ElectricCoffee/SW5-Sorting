ed6d6513aa2face30f822cef75374d3e055a4127
Niko Lepka <slench102+git@gmail.com>
2016-12-13 13:04:45 +0100

    change to add_from_bytes in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #include "product/color.hpp"
 #include "product/size.hpp"
 #include "product/blueprint.hpp"
+#include "product/brick.hpp"
 
 #ifndef UNIT_TEST // important line, do not remove
 #define NO_PIN 0
+
+#define MK_BRK(MR, MG, MB, ML) {MR, MG, MB, ML}
 
 controller contr;
 SFE_ISL29125 sparkfun_sensor; // OBS initialize in main
@@ -18,8 +21,14 @@ motor_pusher *pusher1 = new motor_pusher(NO_PIN, M1, 22, 5000);
 
 // This is a string representation of a randomised brick dataset
 // used for the blueprint to know what data to look for
-char bricks_str[] =
-  "COL:05794119 LEN:03\n";
+// char bricks_str[] =
+//   "COL:05794119 LEN:03\n";
+
+brick_bytes bricks[] = {
+  MK_BRK(0x58, 0x69, 0x47, 3),
+  MK_BRK(0xFF, 0xFF, 0xFF, 4),
+  MK_BRK(0x00, 0x00, 0x00, 2)
+};
 
 
 void setup() {
@@ -34,7 +43,7 @@ void setup() {
   //contr.register_component(pusher1);
 
   blueprint test_blueprint; //dont use here in final build
-  test_blueprint.add_from_string(bricks_str);
+  test_blueprint.add_from_bytes(bricks, 3);
   //test_blueprint.add_from_file("DATA.BRK");
 }
 
